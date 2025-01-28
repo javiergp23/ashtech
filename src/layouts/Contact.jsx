3805d21a85c49f5c789ef89fceb87ad2786e100c
javiergp23 <javgon2306@gmail.com>
2025-01-28 16:22:28 -0300

    add emailJS

--- src/layouts/Contact.jsx
+++ src/layouts/Contact.jsx
@@ -1,8 +1,11 @@
+import { useRef } from 'react';
 import { useLanguage } from '../context/LanguageContext'
+import emailjs from 'emailjs-com';
 import './contact.css'
 
 export default function Contact(){
     const { language } = useLanguage();
+    const form = useRef();
     const texts = {
         es: {
             titleContact: "Contáctate con nosotros",
@@ -25,30 +28,52 @@ export default function Contact(){
             placeholderMessage: "Write a message...",
         },
     };
+
+    const sendEmail = (e) => {
+        e.preventDefault();
+
+        emailjs
+            .sendForm(
+                'service_vb4y59v', // Reemplaza con tu Service ID
+                'template_3tp7det', // Reemplaza con tu Template ID
+                form.current,
+                'M-nG62mrFKNARsnGl' // Reemplaza con tu Public Key de EmailJS
+            )
+            .then(
+                (result) => {
+                    console.log('Email enviado:', result.text);
+                    alert('¡Mensaje enviado con éxito!');
+                },
+                (error) => {
+                    console.error('Error al enviar el email:', error.text);
+                    alert('Ocurrió un error al enviar el mensaje.');
+                }
+            );
+    };
     return(
         <>
             <div className="container-form-component" id="contact">
                 <h1 className="title-form">{texts[language].titleContact}</h1>
                 <h2 className="subtitle-form">{texts[language].subtitleContact}</h2>
-                <form action="" className="form">
+                <form  className="form" ref={form} onSubmit={sendEmail}>
                     <div className="item-form-container item-form-container_contact">
-                        <label htmlFor="">{texts[language].name}</label>
-                         <input type="text" placeholder="    Juan Perez" className="input"/>
+                        <label htmlFor="name">{texts[language].name}</label>
+                         <input type="text" name='user_name' placeholder="    Juan Perez" className="input" required/>
                     </div>
                     <div className="item-form-container item-form-container_contact">
-                        <label htmlFor="">{texts[language].email}</label>
-                         <input type="text" placeholder="    ejemplo@gmail.com" className="input"/>
+                        <label htmlFor="email">{texts[language].email}</label>
+                         <input  type="email" name='user_email' placeholder="    ejemplo@gmail.com" className="input" required/>
                     </div>
                     <div className="item-form-container item-form-container_contact">
-                        <label htmlFor="">{texts[language].telefono}</label>
-                         <input type="text" placeholder="    011 56749288" className="input"/>
+                        <label htmlFor="phone">{texts[language].telefono}</label>
+                         <input type="text" name='user_phone' placeholder="    011 56749288" className="input"/>
                     </div>
                     <div className="item-form-container item-form-container_contact">
-                        <label htmlFor="">{texts[language].mensaje}</label>
-                         <input className="label-menssage input" type="textarea" placeholder={texts[language].placeholderMessage}/>
+                        <label htmlFor="message">{texts[language].mensaje}</label>
+                         <input className="label-menssage input" name='message' type="textarea" placeholder={texts[language].placeholderMessage} required/>
                     </div>
                     <div className="boton-container">
-                        <button className="button-form">{texts[language].buttonContact}</button>
+                        <button type='submit' className="button-form">{texts[language].buttonContact}</button>
                     </div>
                 </form>
             </div>
