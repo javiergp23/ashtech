8270f113d2803edbfa633f9d5de7a3bb30173e5b
javiergp23 <javgon2306@gmail.com>
2025-02-14 15:13:59 -0300

    fix navigation workWithUs

--- src/layouts/WorkWithUs.jsx
+++ src/layouts/WorkWithUs.jsx
@@ -156,141 +156,143 @@ export default function WorkWithUs(){
 
     return(
         <>  
-            <div className='work-section-responsive' id="work" >
-                <h1 className='work-section-text_responsive-title'>
-                    {texts[language].title}
-                </h1>
-                <p className='work-section-text_responsive-subtitle'>
-                    {texts[language].subtitle}
-                </p>
-            </div>
-            <div className="work-section">
-                <div className="work-section-text-container" >
-                    <h1 className="work-title">{texts[language].title}</h1>
-                    <p className="work-section-text">
-                        <span className='work-section-text_responsive-none'>{texts[language].subtitle}</span> <br /><br />
-
-                        {texts[language].paragraphOne} <br /><br />
-
-                        <span className='work-section-text_responsive-none'>{texts[language].paragraphTwo}</span>
+            <div id="work">
+                <div className='work-section-responsive'  >
+                    <h1 className='work-section-text_responsive-title'>
+                        {texts[language].title}
+                    </h1>
+                    <p className='work-section-text_responsive-subtitle'>
+                        {texts[language].subtitle}
                     </p>
-                    <h2 className="sumate">
-                        {texts[language].sum}
-                    </h2>
-                    <button  type="button" className="button-work-with-us" data-bs-toggle="modal" data-bs-target="#exampleModal">
-                        {texts[language].button}
-                    </button>
-                                        {/* MODAL */}
-                    <div className="modal fade" id="exampleModal" tabIndex="-1" aria-labelledby="exampleModalLabel" >
-                        <div className="modal-dialog">
-                            <div className="modal-content">
-                            <div className="modal-header">
-                                <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                            </div>
-                            <div className="modal-body">
-                                <form  className="form" encType="multipart/form-data" method="post" ref={form} onSubmit={handleSubmit(handleFileValidation)}  >
-                                <h1 className="modal-title fs-5 modal-title-cv" id="exampleModalLabel">{texts[language].formTitle}</h1>
-                                    <div className="item-form-container">
-                                        <label className='label-modal_cv' htmlFor="user_name">{texts[language].name}</label>
-                                        <input type="text" name='user_name' placeholder="    Juan Perez" className="input input-modal_cv"
-                                            {...register("user_name", { 
-                                                required: {
-                                                  value: true,
-                                                  message: "El nombre es requerido"
-                                                },
-                                                minLength: {
-                                                  value: 4,
-                                                  message: "El nombre debe tener al menos 4 caracteres"
-                                                },
-                                                maxLength: {
-                                                  value: 40,
-                                                  message: "El nombre no puede tener mas de 40 caracteres"
-                                                }
-                                              })}
+                </div>
+                <div className="work-section">
+                    <div className="work-section-text-container" >
+                        <h1 className="work-title">{texts[language].title}</h1>
+                        <p className="work-section-text">
+                            <span className='work-section-text_responsive-none'>{texts[language].subtitle}</span> <br /><br />
 
-                                        />
-                                        {
-                                            errors.user_name && <p style={{color: "red", fontSize: "15px", textAlign: "left", marginLeft: "45px"}}>{errors.user_name.message}</p>
-                                        }
-                                    </div>
-                                    <div className="item-form-container">
-                                        <label className='label-modal_cv' htmlFor="user_email">{texts[language].email}</label>
-                                        <input type="text" name='user_email' placeholder="    ejemplo@gmail.com" className="input input-modal_cv"
-                                        
-                                        {...register("user_email", { 
-                                            required: {
-                                              value: true,
-                                              message: "El email es requerido"
-                                            },
-                                          
-                                            pattern: {  
-                                              value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
-                                              message: "El email no es valido"
+                            {texts[language].paragraphOne} <br /><br />
+
+                            <span className='work-section-text_responsive-none'>{texts[language].paragraphTwo}</span>
+                        </p>
+                        <h2 className="sumate">
+                            {texts[language].sum}
+                        </h2>
+                        <button  type="button" className="button-work-with-us" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                            {texts[language].button}
+                        </button>
+                                            {/* MODAL */}
+                        <div className="modal fade" id="exampleModal" tabIndex="-1" aria-labelledby="exampleModalLabel" >
+                            <div className="modal-dialog">
+                                <div className="modal-content">
+                                <div className="modal-header">
+                                    <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                </div>
+                                <div className="modal-body">
+                                    <form  className="form" encType="multipart/form-data" method="post" ref={form} onSubmit={handleSubmit(handleFileValidation)}  >
+                                    <h1 className="modal-title fs-5 modal-title-cv" id="exampleModalLabel">{texts[language].formTitle}</h1>
+                                        <div className="item-form-container">
+                                            <label className='label-modal_cv' htmlFor="user_name">{texts[language].name}</label>
+                                            <input type="text" name='user_name' placeholder="    Juan Perez" className="input input-modal_cv"
+                                                {...register("user_name", { 
+                                                    required: {
+                                                    value: true,
+                                                    message: "El nombre es requerido"
+                                                    },
+                                                    minLength: {
+                                                    value: 4,
+                                                    message: "El nombre debe tener al menos 4 caracteres"
+                                                    },
+                                                    maxLength: {
+                                                    value: 40,
+                                                    message: "El nombre no puede tener mas de 40 caracteres"
+                                                    }
+                                                })}
+
+                                            />
+                                            {
+                                                errors.user_name && <p style={{color: "red", fontSize: "15px", textAlign: "left", marginLeft: "45px"}}>{errors.user_name.message}</p>
                                             }
-                                          })}
-                                        
-                                        />
-                                        {
-                                            errors.user_email && <p style={{color: "red", fontSize: "15px", textAlign: "left", marginLeft: "45px"}}>{errors.user_email.message}</p>
-                                        }
-                                    </div>
-                                    <div className="item-form-container">
-                                        <label  className='label-modal_cv' htmlFor="user_phone">{texts[language].phone}</label>
-                                        <input type="number" name='user_phone' placeholder="    011 56749288" className="input input-modal_cv"
-                                            {...register("user_phone", {
-                                                required: "El número de teléfono es requerido",
-                                                pattern: {
-                                                  value: /^(\54\s?9?\s?)?(\d{2,4})\s?\d{3,5}-?\d{4}$/,
-                                                  message: "Formato inválido. Ej: +54 9 11 1234-5678",
-                                                },
-                                                minLength: {
-                                                  value: 10,
-                                                  message: "El número debe tener al menos 10 dígitos",
-                                                },
-                                                maxLength: {
-                                                  value: 15,
-                                                  message: "El número no debe superar los 15 dígitos",
-                                                },
-                                              })}
-                                        
-                                        />
-                                        {
-                                            errors.user_phone && <p style={{color: "red", fontSize: "15px", textAlign: "left", marginLeft: "45px"}}>{errors.user_phone.message}</p>
-                                        }
-                                    </div>
-                                    <div>
-                                        <div className='container-modal-text'>
-                                            <p className='modal-text'> 
-                                            {texts[language].validation}
-                                            </p>
                                         </div>
-                                        <div className='container-modal-img'>
-                                            <img className='modal-img' src="/adjuntar-archivo.png" alt="adj-png" />
-                                            <label className='modal-text-adj custom-file-label' htmlFor="file">{texts[language].adj}
-                                            </label>
-                                            <input className='transparent-file-input' name='user_file' type="file" id="file" accept=".pdf"  onChange={handleFileChange}
-                                             />
-                                        </div>
-                                        {errors.user_file && <p style={{color: "red", fontSize: "15px", textAlign: "left", marginLeft: "20px"}}>{errors.user_file.message}</p>}
+                                        <div className="item-form-container">
+                                            <label className='label-modal_cv' htmlFor="user_email">{texts[language].email}</label>
+                                            <input type="text" name='user_email' placeholder="    ejemplo@gmail.com" className="input input-modal_cv"
                                             
-                                        {uploading && <p className='upload-message'>Subiendo archivo...</p>}
-                                        {uploadSuccess && <p className='upload-confirm' style={{ color: "green"}}> {file?.name}  ✅</p>}
-                                    </div>
-                                    <div className='container-button-modal'>    
-                                        <button  type='submit' className="button-work-with-us_modal" {...(allowCloseModal && { "data-bs-toggle": "modal", "data-bs-target": "#exampleModal" })} >
-                                            {texts[language].buttonForm}
-                                        </button>
-                                    </div>
-                                </form>
-                            </div>
-                            
+                                            {...register("user_email", { 
+                                                required: {
+                                                value: true,
+                                                message: "El email es requerido"
+                                                },
+                                            
+                                                pattern: {  
+                                                value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                                                message: "El email no es valido"
+                                                }
+                                            })}
+                                            
+                                            />
+                                            {
+                                                errors.user_email && <p style={{color: "red", fontSize: "15px", textAlign: "left", marginLeft: "45px"}}>{errors.user_email.message}</p>
+                                            }
+                                        </div>
+                                        <div className="item-form-container">
+                                            <label  className='label-modal_cv' htmlFor="user_phone">{texts[language].phone}</label>
+                                            <input type="number" name='user_phone' placeholder="    011 56749288" className="input input-modal_cv"
+                                                {...register("user_phone", {
+                                                    required: "El número de teléfono es requerido",
+                                                    pattern: {
+                                                    value: /^(\54\s?9?\s?)?(\d{2,4})\s?\d{3,5}-?\d{4}$/,
+                                                    message: "Formato inválido. Ej: +54 9 11 1234-5678",
+                                                    },
+                                                    minLength: {
+                                                    value: 10,
+                                                    message: "El número debe tener al menos 10 dígitos",
+                                                    },
+                                                    maxLength: {
+                                                    value: 15,
+                                                    message: "El número no debe superar los 15 dígitos",
+                                                    },
+                                                })}
+                                            
+                                            />
+                                            {
+                                                errors.user_phone && <p style={{color: "red", fontSize: "15px", textAlign: "left", marginLeft: "45px"}}>{errors.user_phone.message}</p>
+                                            }
+                                        </div>
+                                        <div>
+                                            <div className='container-modal-text'>
+                                                <p className='modal-text'> 
+                                                {texts[language].validation}
+                                                </p>
+                                            </div>
+                                            <div className='container-modal-img'>
+                                                <img className='modal-img' src="/adjuntar-archivo.png" alt="adj-png" />
+                                                <label className='modal-text-adj custom-file-label' htmlFor="file">{texts[language].adj}
+                                                </label>
+                                                <input className='transparent-file-input' name='user_file' type="file" id="file" accept=".pdf"  onChange={handleFileChange}
+                                                />
+                                            </div>
+                                            {errors.user_file && <p style={{color: "red", fontSize: "15px", textAlign: "left", marginLeft: "20px"}}>{errors.user_file.message}</p>}
+                                                
+                                            {uploading && <p className='upload-message'>Subiendo archivo...</p>}
+                                            {uploadSuccess && <p className='upload-confirm' style={{ color: "green"}}> {file?.name}  ✅</p>}
+                                        </div>
+                                        <div className='container-button-modal'>    
+                                            <button  type='submit' className="button-work-with-us_modal" {...(allowCloseModal && { "data-bs-toggle": "modal", "data-bs-target": "#exampleModal" })} >
+                                                {texts[language].buttonForm}
+                                            </button>
+                                        </div>
+                                    </form>
+                                </div>
+                                
+                                </div>
                             </div>
                         </div>
                     </div>
-                </div>
-                <div className="work-img">
-                    <img className="work-section_img" src="/img-work-with-us.png" alt="img-section-about" />
-                    <img className="work-section_img-responsive" src="/img-work-section-responsive.png" alt="img-section-about-responsive" />
+                    <div className="work-img">
+                        <img className="work-section_img" src="/img-work-with-us.png" alt="img-section-about" />
+                        <img className="work-section_img-responsive" src="/img-work-section-responsive.png" alt="img-section-about-responsive" />
+                    </div>
                 </div>
             </div>
         </>
