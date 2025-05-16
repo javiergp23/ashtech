13b4f4ef4776ca9a444152af9fa0f38ce5a1a88e
javiergp23 <javgon2306@gmail.com>
2025-05-16 09:18:46 -0300

    update

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -4,7 +4,7 @@ import { createClient } from "@supabase/supabase-js";
 const supabaseUrl = import.meta.env.REACT_APP_SUPABASE_URL;
 const supabaseAnonKey = import.meta.env.REACT_APP_SUPABASE_ANON_KEY;
 
-// Crear cliente de Supabase
+// cliente de Supabase
 const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
 export default supabase;
